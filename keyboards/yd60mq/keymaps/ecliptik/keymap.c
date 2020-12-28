52f132e5b68312aa0ae4c17352c1c92773a5c832
Micheal Waltz <micheal@ecliptik.com>
2020-12-28 09:13:10 -0800

    Update to use tap layer

--- keyboards/yd60mq/keymaps/ecliptik/keymap.c
+++ keyboards/yd60mq/keymaps/ecliptik/keymap.c
@@ -2,7 +2,7 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define NUMB 2 // numbers
+#define NAV 2 // numbers
 
 enum custom_keycodes {
   SCR_SHOT,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO, KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO, KC_RSFT,   KC_NO,
-        KC_LCTL, KC_LALT, KC_LGUI,                   MO(1),   LT(NUMB, KC_SPC),  MO(1),                     KC_RALT, KC_RCTL, KC_LEFT, SCR_SHOT, REC_VID
+        KC_LCTL, KC_LALT, KC_LGUI,                   MO(1),   LT(NAV, KC_SPC),  MO(1),                     TT(NAV), KC_RCTL, KC_LEFT, SCR_SHOT, REC_VID
     ),
 
 [SYMB] = LAYOUT_all(
@@ -94,15 +94,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-[NUMB] = LAYOUT_all(
+[NAV] = LAYOUT_all(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
         _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, KC_HOME, RGB_VAI, RGB_VAD, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, _______, _______, _______,
         _______, _______, _______, _______, BL_DEC,  KC_HOME, KC_PGDOWN,  KC_PGUP, KC_END, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 
 };
