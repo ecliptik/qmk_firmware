7ef0ecd7cf23e0bc6a30ba3d00eeef04c3def63d
Micheal Waltz <micheal@ecliptik.com>
2020-12-27 23:32:08 -0800

    Modify nav layer for yd60mq

--- keyboards/yd60mq/keymaps/ecliptik/keymap.c
+++ keyboards/yd60mq/keymaps/ecliptik/keymap.c
@@ -99,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [NUMB] = LAYOUT_all(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, KC_HOME, RGB_VAI, RGB_VAD, _______, _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, _______, _______, _______,
+        _______, _______, _______, _______, BL_DEC,  KC_HOME, KC_PGDOWN,  KC_PGUP, KC_END, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______
     )
 
